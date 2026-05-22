0787ed56115cd2972b08d615f51279399c85c07d
Felipe Borges <lipelsborges@gmail.com>
2026-05-22 14:17:26 -0300

    corrige rota de alunos para usar o método correto e adiciona comentários para clareza

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\ExerciciosController;
 use App\Http\Controllers\PlanosController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 //layout
 Route::get('/', [SiteController::class, 'layout'])->name('index_page');
 
 // Rota para a página de alunos
-Route::get('/aluno', [SiteController::class, 'aluno'])->name('alunos.index');
-Route::get('/aluno/cadastrar', [AlunosController::class, 'cadastrarAluno'])->name('alunos.cadastrar');
+Route::get('/aluno', [AlunosController::class, 'aluno'])->name('alunos.index');
+Route::get('/aluno/cadastrar', [AlunosController::class, 'cadastrarAlunoView'])->name('alunos.cadastrar');
+Route::post('/aluno/cadastrarAluno', [AlunosController::class, 'cadastrarAluno'])->name('alunos.cadastrarAluno');
 Route::get('/aluno/editar', [AlunosController::class, 'editarAluno'])->name('alunos.editar');
 
 
@@ -28,3 +31,6 @@ Route::get('/exercicios/editar', [ExerciciosController::class, 'editar'])->name(
 Route::get('/planos', [PlanosController::class, 'index'])->name('planos.index');
 Route::get('/planos/cadastrar', [PlanosController::class, 'cadastrar'])->name('planos.cadastrar');
 Route::get('/planos/editar', [PlanosController::class, 'editar'])->name('planos.editar');
+
+
+// Route::get('/users', [UserController::class, 'index']);
