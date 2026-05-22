a04b45a055d076f77f6346c4bd15967ed1557113
Felipe Borges <lipelsborges@gmail.com>
2026-05-22 14:16:58 -0300

    refatora AlunosController para incluir método aluno e reorganizar métodos de cadastro

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Alunos;
 use Illuminate\Http\Request;
 
 class AlunosController extends Controller
 {
-    public function cadastrarAluno()
+
+    public function aluno()
+    {
+        $alunos = Alunos::all();
+
+        return view('alunos.index', compact('alunos'));
+    }
+
+    public function cadastrarAlunoView($dados = null)
     {
         return view('alunos.cadastrar');
+    }
+
+    public function cadastrarAluno(Request $request)
+    {
+        $dados = $request->all();
+        $dados['email'] = 'felipe@gmail.com';
+
+        Alunos::create($dados);
+
+        return redirect()->route('alunos.index');
     }
 
     public function editarAluno()
